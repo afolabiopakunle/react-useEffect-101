6fc48df9d3423fc18d96f0f1f570b38f604af796
afolabiopakunle <opakunleafolabi@gmail.com>
2022-10-09 17:57:34 +0100

    bootstrap and states set

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React, {useState} from "react";
 import "./style.css";
 
 export default function App() {
-  const [resourceType, setResourceType] = useState('post')
+  const [resourceType, setResourceType] = useState('post');
   return (
     <div>
-      
+      <div className="container p-3">
+      <div className='btn-group'>
+          <button className='btn btn-secondary'>Posts</button>
+          <button className='btn btn-warning'>Resources</button>
+          <button className='btn btn-danger'>Resources</button>
+        </div>
+      </div>
     </div>
   );
 }
